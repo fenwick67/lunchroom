2b829d4c24661f6a7f7fabbdd45b8be95055a4c9
fenwick67 <smileman66@gmail.com>
2015-10-18 20:52:33 -0500

    layout updates

--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
+
+var myLineChart;
 $(function(){
   
 var timeMin = 690
@@ -5,19 +7,9 @@ var timeMin = 690
 
 var dirty = false;
 
-function timeToText(time){
-  if (typeof time == 'string'){
-    time = parseInt(time,10);
-  }
-  var hours
-    , minutes;
-  hours = (Math.floor(time/60)-1 )% 12 + 1;
-  minutes = (time % 60).toString();
-  while(minutes.length < 2){
-    minutes = '0' + minutes;
-  }
-  return hours + ':' + minutes;
-}
+var ctx = document.getElementById("mychart").getContext("2d");
+
+
 
 $('#time').on('input', function(e) {
   dirty = true;
@@ -30,19 +22,19 @@ $('#wait').on('input', function(e) {
     desc = "";
 
   if (wait == 0) {
-    desc = "no wait at all";
+    desc = "none";
   } else if (wait < 20){
-    desc = "pretty quick wait"
+    desc = "pretty quick"
   }else if (wait < 40 ) {
-    desc = "short wait";
+    desc = "short";
   } else if (wait < 60) {
-    desc = "normal wait";
+    desc = "normal";
   } else if (wait < 80) {
-    desc = "longer wait";
+    desc = "longer";
   } else if (wait <100) {
-    desc = "pretty long wait"
+    desc = "pretty long"
   }else {
-    desc = "crazy wait"
+    desc = "crazy"
   }
 
   $('#waitdisplay').html(desc);
@@ -82,90 +74,99 @@ $.ajax({
 });
 
 function updateChart(data){
-    var todaysForecast = data.today;
-    
-    var labels60 = [];
-    for(var i = 0; i <= (timeMax - timeMin); i++){
-      if(i% 5 === 0){
-        labels60.push(timeToText(i+timeMin));
-      }else{
-        labels60.push(timeToText(i+timeMin).split(':')[1]);
-      }
+  var todaysForecast = data.today;
+  
+  var labels60 = [];
+  for(var i = 0; i <= (timeMax - timeMin); i++){
+    if(i% 5 === 0){
+      labels60.push(timeToText(i+timeMin));
+    }else{
+      //labels60.push(timeToText(i+timeMin).split(':')[1]);
+      labels60.push('');
     }
+  }
+  
+  var chartData = {
+    labels: labels60,
+    datasets: [{
+      label: "Estimated Wait",
+      fillColor: "rgba(255,187,0,0.2)",
+      strokeColor: "#fb0",
+      pointColor: "#fb0",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "#fb0",
+      data: todaysForecast
+    }]
+  };
+  var options = {
+
+    //Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines: true,
+
+    //String - Colour of the grid lines
+    scaleGridLineColor: "rgba(0,0,0,.05)",
+
+    //Number - Width of the grid lines
+    scaleGridLineWidth: 1,
     
-    var chartData = {
-      labels: labels60,
-      datasets: [{
-        label: "Estimated Wait",
-        fillColor: "rgba(255,187,0,0.2)",
-        strokeColor: "#fb0",
-        pointColor: "#fb0",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "#fb0",
-        data: todaysForecast
-      }]
-    };
-    var options = {
+    //Boolean, show sscale labels or not:
+    scaleShowLabels:true,
+    
+    //respond to resizing
+    responsive: true,
+    //overriding the scaleGridLineColor
+    scaleOverride:true,
+    scaleSteps:1,
+    scaleStepWidth:100,
+    scaleStartValue:0,
+    scaleLabel:'<%=value==0?"Short":"Long"%>',
+   
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: false,
 
-      //Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines: true,
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: true,
 
-      //String - Colour of the grid lines
-      scaleGridLineColor: "rgba(0,0,0,.05)",
+    //Boolean - Whether the line is curved between points
+    bezierCurve: true,
 
-      //Number - Width of the grid lines
-      scaleGridLineWidth: 1,
-      
-      //Boolean, show sscale labels or not:
-      scaleShowLabels:true,
-      
-      //overriding the scaleGridLineColor
-      scaleOverride:true,
-      scaleSteps:1,
-      scaleStepWidth:100,
-      scaleStartValue:0,
-      scaleLabel:'<%=value==0?"Short":"Long"%>',
-     
-      //Boolean - Whether to show horizontal lines (except X axis)
-      scaleShowHorizontalLines: false,
+    //Number - Tension of the bezier curve between points
+    bezierCurveTension: 0.4,
 
-      //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines: true,
+    //Boolean - Whether to show a dot for each point
+    pointDot: false,
 
-      //Boolean - Whether the line is curved between points
-      bezierCurve: true,
+    //Number - Radius of each point dot in pixels
+    pointDotRadius: 4,
 
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension: 0.4,
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth: 1,
 
-      //Boolean - Whether to show a dot for each point
-      pointDot: false,
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius: 0,
 
-      //Number - Radius of each point dot in pixels
-      pointDotRadius: 4,
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke: true,
 
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth: 1,
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth: 2,
 
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius: 0,
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill: true,
 
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke: true,
+    //String - A legend template
+    //legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+  };
 
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth: 2,
-
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill: true,
-
-      //String - A legend template
-      legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-    };
-
-    var myLineChart = new Chart(ctx).Line(chartData, options);
-
+  myLineChart = new Chart(ctx).Line(chartData, options);
+  
+  resizeChart();
+  function resizeChart(){
+    //$('#chartcontainer').width($('.topper').width() - 40)
+    myLineChart.resize(myLineChart.render,true);
+  }
+  window.setInterval(resizeChart,500);
   
 }
 
@@ -174,6 +175,18 @@ function ajaxFail(){
 }
 
 
-var ctx = document.getElementById("mychart").getContext("2d");
-  
 });
+
+function timeToText(time){
+  if (typeof time == 'string'){
+    time = parseInt(time,10);
+  }
+  var hours
+    , minutes;
+  hours = (Math.floor(time/60)-1 )% 12 + 1;
+  minutes = (time % 60).toString();
+  while(minutes.length < 2){
+    minutes = '0' + minutes;
+  }
+  return hours + ':' + minutes;
+}
